6838ee43462185327d8a554e580d7ab168919279
knova <kiyuzanova@hotmail.com>
2013-02-17 22:35:48 +1100

    in which navigation actually generates now (yes!)
    ...with only 1 loop. dohohohoh~

--- server.py
+++ server.py
@@ -7,14 +7,27 @@ import sys
 sys.path.append(path.dirname(path.realpath(__file__)))
 
 def index(response):
-    #con = sqlite3.connect("models/data.db")
-    #cur = con.cursor()
-    #cur.execute('SELECT NAME FROM Navigation ORDER BY ord;')
-    #row = cur.fetchall()
-    values = {"row": "derp"}
+    con = sqlite3.connect("models/data.db")
+    cur = con.cursor()
+    cur.execute('SELECT NAME FROM Navigation ORDER BY ord;')
+    row = cur.fetchall()
+    navigation = ""
+    for entry in row:
+        entry = "".join(entry)
+        cur.execute('SELECT NAME FROM Pages WHERE CATEGORY = ? ORDER BY ord;', [entry])
+        submenu = cur.fetchall()
+        if submenu:
+            submenu = "<li>" + "</li><li>".join(["".join(a) for a in submenu]) + "</li>"
+        else:
+            submenu = ""
+        submenu = """<ul class='navi'>
+            <li class='navi-heading'><em>""" + entry + "</em></li>"  + submenu + "</li></ul>"
+        navigation += (submenu)
+    print(navigation)
+    values = {"derp": navigation}
     #print(row)
-    render(response, 'test.html', values)
-    #con.close()
+    render('test.html', response, values)
+    con.close()
 
 if __name__ == "__main__":
     server = Server()
